950c8a920d074f573a5d8e100e384c5f62016b48
1487quantum <1487quantum@users.noreply.github.com>
2022-07-27 11:45:45 +0800

    Change to realtime

--- src/gazebo_ros_realsense.cpp
+++ src/gazebo_ros_realsense.cpp
@@ -57,7 +57,8 @@ void GazeboRosRealsense::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
 
 void GazeboRosRealsense::OnNewFrame(const rendering::CameraPtr cam,
                                     const transport::PublisherPtr pub) {
-  common::Time current_time = this->world->SimTime();
+  //common::Time current_time = this->world->SimTime();
+  common::Time current_time = this->world->RealTime();
 
   // identify camera
   std::string camera_id = extractCameraName(cam->Name());
